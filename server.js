9c2e81cf81af58b54b95ee90a65857e0715238bf
Banesa Guaderrama <banesa.guaderrama@hotmail.com>
2018-11-15 12:03:06 -0500

    DB

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 //update with mylab user and password//
-mongoose.connect('mongodb://socketapp:d1234sk@ds137483.mlab.com:37483/heroku_78t5nvpt', { useNewUrlParser: true });
+mongoose.connect('mongodb://socketapp:1234sk@ds137483.mlab.com:37483/heroku_78t5nvpt', { useNewUrlParser: true });
 
 require('./sockets/todo-sockets')(io);
 require('./routes/api-routes.js')(app);
